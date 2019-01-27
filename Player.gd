8afcd2ac3e4f7a3ab5e034ad9b45971ea8c0730d
lilak <kcabrak@gmail.com>
2019-01-27 01:14:06 -0300

    moving breaks heal

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var tile_size = 64
+var stunned = false
 
 var sanity = 100 # (current sanity)
 var sanity_recharge = 30
@@ -8,6 +9,7 @@ var max_sanity = 100
 var sanity_step = 5
 var sanity_hit = 20
 var n_pos
+var m_pos
 
 func _ready():
 	n_pos = position
@@ -27,7 +29,7 @@ func _physics_process(delta):
 	elif Input.is_action_just_pressed("ui_down"):
 		$Sprite.animation = "down"
 		move_vec.y += tile_size
-	if (!move_and_collide(move_vec, true, true, true) or 
+	if !stunned and (!move_and_collide(move_vec, true, true, true) or 
 			move_and_collide(move_vec, true, true, true).collider.name != "Walls"):
 		move_and_collide(move_vec)
 	
@@ -35,7 +37,9 @@ func _physics_process(delta):
 	if position != n_pos:
 		sanity -= sanity_step
 		n_pos = position
-	if Input.is_action_pressed("ui_focus"):
+	if Input.is_action_just_pressed("ui_focus"):
+		m_pos = position
+	elif Input.is_action_pressed("ui_focus") and m_pos == position:
 		self.modulate = Color(0.7, 6, 6)
 		if sanity < max_sanity:
 			sanity += delta*sanity_recharge
@@ -49,6 +53,7 @@ func _physics_process(delta):
 		$SanityRecharge.stop()
 
 func hit():
+	stunned = true
 	$Sprite.modulate = Color(100, 100, 100)
 	sanity -= 20
 	match $Sprite.animation:
@@ -62,6 +67,7 @@ func hit():
 			move_and_collide( Vector2(tile_size, 0))
 			
 	yield(get_tree().create_timer(0.5), "timeout")
+	stunned = false
 	$Sprite.modulate = Color(1, 1, 1)
 
 
