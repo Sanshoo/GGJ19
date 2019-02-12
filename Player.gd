fe6f7d50076c58e65e3e76562d5947b2b871a06e
lilak <kcabrak@gmail.com>
2019-02-12 11:41:23 -0300

    added hold to walk

--- Player.gd
+++ Player.gd
@@ -27,19 +27,19 @@ func _physics_process(delta):
 	# movement
 	var move_vec = Vector2(0, 0)
 	if movable:
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_pressed("ui_up"):
 			$Sprite.animation = "up"
 			to_be_moved = true
 			move_vec.y -= tile_size
-		elif Input.is_action_just_pressed("ui_left"):
+		elif Input.is_action_pressed("ui_left"):
 			$Sprite.animation = "left"
 			to_be_moved = true
 			move_vec.x -= tile_size
-		elif Input.is_action_just_pressed("ui_right"):
+		elif Input.is_action_pressed("ui_right"):
 			$Sprite.animation = "right"
 			to_be_moved = true
 			move_vec.x += tile_size
-		elif Input.is_action_just_pressed("ui_down"):
+		elif Input.is_action_pressed("ui_down"):
 			$Sprite.animation = "down"
 			to_be_moved = true
 			move_vec.y += tile_size
